1378237eedbdc193d620334b595efe93996fad0b
Eduardo Domasig <edomasig@deloitte.com>
2021-06-24 16:21:24 +1000

    End of session 10

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,10 +1,12 @@
 using System;
 
+
 namespace Domain
 {
     public class Activity
     {
         public Guid Id { get; set; }
+       
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
