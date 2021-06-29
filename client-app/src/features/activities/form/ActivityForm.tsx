f215369aaefb4d1bb9e5455214508d049649c396
Eduardo Domasig <edomasig@deloitte.com>
2021-06-29 19:15:14 +1000

    End section 15

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,24 +12,16 @@ import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import { categoryOptions } from '../../../app/common/options/categoryOptions';
 import MyDateInput from '../../../app/common/form/MyDateInput';
-import { Activity } from '../../../app/models/activity';
+import { ActivityFormValues } from '../../../app/models/activity';
 
 
 export default observer (function ActivityForm() {
 
  const history = useHistory();
  const {activityStore} = useStore(); 
- const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore; 
+ const {createActivity, updateActivity, loadActivity, loadingInitial} = activityStore; 
  const {id} = useParams<{id: string}>(); 
- const [activity, setActivity] = useState<Activity>({
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    city: '',
-    venue: '',
-    date:null
- }); 
+ const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues()); 
 
  const validationSchema = Yup.object({
      title: Yup.string().required('Title is required'),
@@ -41,13 +33,13 @@ export default observer (function ActivityForm() {
  })
 
  useEffect(() => {
-    if (id) loadActivity(id).then(activity => setActivity(activity!)); 
+    if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity))); 
     
   }, [id, loadActivity]) 
 
 
-    function handleFormSubmit(activity: Activity){
-         if (activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues){
+         if (!activity.id) {
             let newActivity = {
                 ...activity,
                 id: uuid()
@@ -65,7 +57,8 @@ export default observer (function ActivityForm() {
     return (
         <Segment clearing>
             <Header content='Activity Details' sub color='teal' />
-            <Formik validationSchema={validationSchema } enableReinitialize initialValues={activity} onSubmit={values => handleFormSubmit(values)} >
+            <Formik validationSchema={validationSchema } enableReinitialize initialValues={activity} 
+                onSubmit={values => handleFormSubmit(values)}>
                 {({handleSubmit, isValid, isSubmitting, dirty}) => (
                      <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                         <MyTextInput name='title' placeholder='Title' />
@@ -86,7 +79,7 @@ export default observer (function ActivityForm() {
                             floated='right' 
                             positive type='submit' 
                             content='Submit'
-                            loading={loading}
+                            loading={isSubmitting}
                         />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
