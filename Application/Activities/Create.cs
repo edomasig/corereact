9a2ccbb01935cfc13d14cdc0ab0b72d01cfbb1d2
Eduardo Domasig <edomasig@deloitte.com>
2021-06-29 16:42:52 +1000

    End section 14

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,18 +1,20 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest<Result<Unit>> 
+        public class Command : IRequest<Result<Unit>>
         {
-            public Activity Activity {get; set;}
+            public Activity Activity { get; set; }
         }
 
         public class CommandValidator : AbstractValidator<Command>
@@ -26,16 +28,30 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
-            public async Task< Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
-                _context.Activities.Add(request.Activity); 
 
-                var result =  await _context.SaveChangesAsync() > 0;
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUserName());
+
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user, 
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+
+                request.Activity.Attendees.Add(attendee);
+
+                _context.Activities.Add(request.Activity);
+
+                var result = await _context.SaveChangesAsync() > 0;
 
                 if (!result) return Result<Unit>.Failure("Failed to create activity");
 
